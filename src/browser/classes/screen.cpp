db71469dbe622f9fae9f5f30dd334b79bd09df64
vitawrap <thevitawrap@gmail.com>
2026-04-29 16:01:48 +0200

    Screen: Start basic ops

--- src/browser/classes/screen.cpp
+++ src/browser/classes/screen.cpp
@@ -1,5 +1,6 @@
 #include "screen.hpp"
 #include "core/app.hpp"
+#include <algorithm>
 
 namespace ms {
 namespace browser {
@@ -7,6 +8,43 @@ namespace browser {
     void Screen::initContext() {
         auto* browser = Application::get()->getBrowserContext();
         m_canvas = browser->getCanvas();
+        m_canvas->push();
+        m_canvas->translate(m_canvas->getWidth() >> 1, m_canvas->getHeight() >> 1);
+        float ratio = std::min(m_canvas->getWidth() / 200, m_canvas->getHeight() / 200);
+        m_canvas->scale(ratio, ratio);
+        m_width = m_canvas->getWidth() / ratio;
+        m_height = m_canvas->getHeight() / ratio;
+    }
+
+    void Screen::setColor(uint32_t color) {
+        /** TODO: */
+    }
+
+    void Screen::clear(uint32_t color) {
+        /** TODO: */
+    }
+
+    uint32_t Screen::stringToColor(char const* str) {
+        int r, g, b;
+        if (sscanf(str, "rgb(%d ,%d ,%d)", &r, &g, &b) == 3) {
+            return (((r & 255) << 16) | ((g & 255) << 8) | (b & 255)) | 0xFF000000;
+        }
+        int len = 0;
+        if (sscanf(str, "#%X%n", &r, &len)) {
+            if (len == 4) { // #RGB notation
+                return r << 12 | r << 8 | ((r & 7) << 4);
+            } else if (len == 7) { // #RRGGBB notation
+                return (r & 0xFFFFFF) | 0xFF000000;
+            }
+        }
+        return 0u;
+    }
+
+    uint32_t Screen::decimalToColor(int dec) {
+      int r = (((dec / 100) % 10) / 9) * 255;
+      int g = (((dec / 10) % 10) / 9) * 255;
+      int b = ((dec % 10) / 9) * 255;
+      return (r << 16) + (g << 8) + b;
     }
 
 }
--- src/browser/classes/screen.hpp
+++ src/browser/classes/screen.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include "graphics/canvas2d_rc.hpp"
 
 namespace ms {
@@ -10,10 +11,25 @@ namespace browser {
      */
     class Screen {
         gfx::CanvasRC2D* m_canvas;
+
+        /** Virtual width and height */
+        int m_width, m_height;
     
     public:
         /** Grab context */
         void initContext();
+
+        /** Clear with color */
+        void clear(uint32_t color);
+
+        /** Set color state */
+        void setColor(uint32_t color);
+
+        /** Translate a color notation string to an actual color */
+        static uint32_t stringToColor(char const* str);
+
+        /** Short decimal notation (000-999) to conventional color */
+        static uint32_t decimalToColor(int dec);
         
     };
 
