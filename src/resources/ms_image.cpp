da1e912f49ffcb550a27263613e39f46b7f0ef0c
vitawrap <thevitawrap@gmail.com>
2026-05-12 11:20:21 +0200

    Image: Atlas creator with rectpack2D

--- src/resources/ms_image.cpp
+++ src/resources/ms_image.cpp
@@ -1,12 +1,35 @@
 #include "ms_image.hpp"
+#include "core/util.hpp"
 
 #include <raylib.h>
 #include <memory.h>
+#include <rectpack2D/finders_interface.h>
 
+namespace rp = rectpack2D;
 using PlatformImage = ::Image;
 
 namespace ms {
 namespace res {
+
+    using spaces_type = rp::empty_spaces<false, rp::default_empty_spaces>;
+    using rect_type = rp::output_rect_t<spaces_type>;
+
+    struct ImageRect {
+        rect_type rect;
+        Image const* image;
+
+        ImageRect(Image const* img, const rect_type& rect_) :
+            rect(rect_), image(img) { }
+
+        // in-place mutator for rectpack2D
+        auto& get_rect() {
+            return rect;
+        }
+
+        const auto& get_rect() const {
+            return rect;
+        }
+    };
 
     Image::~Image() {
         PlatformImage& image = *reinterpret_cast<PlatformImage*>(m_internalImage);
@@ -59,6 +82,74 @@ namespace res {
         if (IsImageValid(image))
             return image.height;
         return 0;
+    }
+
+    ResourceHandle<Image> Image::createAtlas(Image const** images, size_t num, char const* atlasCacheName) {
+        int binSide = 256;
+        std::vector<ImageRect> rectangles;
+
+        for (int i = 0; i < num; ++i) {
+            Image const* image = images[i];
+            rectangles.emplace_back(image, rp::rect_xywh(0, 0, image->getWidth(), image->getHeight()));
+        }
+
+        bool packingFailed;
+
+        auto successHandler = [](rect_type&) {
+            return rp::callback_result::CONTINUE_PACKING;
+        };
+
+        auto failHandler = [&packingFailed](rect_type&) {
+            packingFailed = true;
+            return rp::callback_result::ABORT_PACKING;
+        };
+
+        const int discardStep = -4;
+        while (true) {
+            packingFailed = false;
+            const rp::rect_wh resultSize = rp::find_best_packing<spaces_type>(
+                rectangles,
+                make_finder_input(
+                    binSide,
+                    discardStep,
+                    successHandler,
+                    failHandler,
+                    rp::flipping_option::DISABLED
+                )
+            );
+            binSide <<= 1;
+            DEBUG_ASSERT(binSide <= 2048);
+
+            if (!packingFailed) {
+                PlatformImage* pImage = new PlatformImage;
+                Image* resImage = new Image(pImage);
+                resImage->m_path = atlasCacheName;
+                
+                PlatformImage bin = GenImageColor(resultSize.w, resultSize.h, ::Color{0,0,0,0});
+                for (const auto& rect : rectangles) {
+                    const auto& rpRect = rect.get_rect();
+                    const Rectangle srcRect = Rectangle{
+                        0, 0, 
+                        static_cast<float>(rect.image->getWidth()), static_cast<float>(rect.image->getHeight())
+                    };
+                    const Rectangle dstRect = Rectangle{
+                        static_cast<float>(rpRect.x), static_cast<float>(rpRect.y),
+                        static_cast<float>(rpRect.w), static_cast<float>(rpRect.h)};
+                    ImageDraw(&bin, *(PlatformImage*)(rect.image->getPlatformImage()), srcRect, dstRect, WHITE);
+
+                    // add to image resource atlas rectangles
+                    resImage->m_atlasRects[rect.image->getPath()] = AtlasRect{rpRect.x, rpRect.y, rpRect.w, rpRect.h};
+                }
+                //ExportImage(bin, "./atlas.png"); // debug atlas creation
+
+                // do not unload bin Image, it gets transferred into pImage.
+                memcpy((void*)pImage, &bin, sizeof(PlatformImage));
+                return ResourceManager::get()->cacheResource<Image>(atlasCacheName, resImage);
+            }
+        }
+
+        // failed...
+        return ResourceHandle<Image>{nullptr};
     }
 }
 }
--- src/resources/ms_image.hpp
+++ src/resources/ms_image.hpp
@@ -1,12 +1,25 @@
 #pragma once
 
 #include "resources/resource.hpp"
+#include "resource_manager.hpp"
+#include "core/robin_hood.hpp"
 #include "io/file.hpp"
 
 namespace ms {
 namespace res {
 
     class Image : public Resource {
+    public:
+        struct AtlasRect {
+            int x, y, width, height;
+        };
+
+    protected:
+        robin_hood::unordered_map<
+            std::string,
+            AtlasRect
+        > m_atlasRects;
+
         std::string m_path;
         void* m_internalImage;
 
@@ -24,6 +37,19 @@ namespace res {
 
         size_t getWidth() const;
         size_t getHeight() const;
+
+        /** Create and cache a new image resource from multiple images, visually containing all of them */
+        static ResourceHandle<Image> createAtlas(Image const** images, size_t num, char const* atlasCacheName);
+
+        bool isAtlas() const { return m_atlasRects.size(); }
+        bool findAtlasRect(std::string const& name, AtlasRect& outRect) {
+            auto itr = m_atlasRects.find(name);
+            if (itr != m_atlasRects.end()) {
+                outRect = itr->second;
+                return true;
+            }
+            return false;
+        }
 
         DECLARE_LOADER;
     };
