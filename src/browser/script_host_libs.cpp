20ad9703b304a2bc0186d9b0edefe5cc3c8d1ab6
vitawrap <thevitawrap@gmail.com>
2026-05-13 15:55:34 +0200

    Console: Error logger

--- src/browser/script_host_libs.cpp
+++ src/browser/script_host_libs.cpp
@@ -331,7 +331,8 @@ namespace browser {
         JSTempVal globalThis = JS_GetGlobalObject(ctx);
         JSValue console = JS_NewObject(ctx);
         auto output = [](JSContext *ctx, JSValueConst self, int argc, JSValueConst *argv, int magic) -> JSValue {
-            LOG_MSGF("[CONSOLE.%s] ", magic? "INFO" : "LOG");
+            char const* fnName[] = {"INFO", "LOG", "ERROR"};
+            LOG_MSGF("[CONSOLE.%s] ", fnName[magic]);
             for (int i = 0; i < argc; ++i) {
                 char const* str = JS_ToCString(ctx, argv[i]);
                 LOG_MSGF("%s ", str);
@@ -342,8 +343,10 @@ namespace browser {
         };
         JSValue log = JS_NewCFunctionMagic(ctx, output, "log", 1, JSCFunctionEnum::JS_CFUNC_generic_magic, 0);
         JSValue info = JS_NewCFunctionMagic(ctx, output, "info", 1, JSCFunctionEnum::JS_CFUNC_generic_magic, 1);
+        JSValue error = JS_NewCFunctionMagic(ctx, output, "error", 1, JSCFunctionEnum::JS_CFUNC_generic_magic, 2);
         JS_SetPropertyStr(ctx, console, "log", log);
         JS_SetPropertyStr(ctx, console, "info", info);
+        JS_SetPropertyStr(ctx, console, "error", error);
         JS_SetPropertyStr(ctx, globalThis, "console", console);
     }
 
