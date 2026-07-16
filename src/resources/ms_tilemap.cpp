d8432a7685a99354a0ff4e43a070545d19c382f3
vitawrap <thevitawrap@gmail.com>
2026-07-16 11:31:24 +0200

    TileMap: Resource for JSON map

--- src/resources/ms_tilemap.cpp
+++ src/resources/ms_tilemap.cpp
@@ -1,10 +1,80 @@
 #include "ms_tilemap.hpp"
+#include "io/json.hpp"
+#include <algorithm>
 
 namespace ms {
 namespace res {
 
+    const TileMap::Tile TileMap::EMPTY_TILE = { 0, 0, 0 };
+
+    TileMap::TileMap():
+        m_width(0), m_height(0), m_blockWidth(0), m_blockHeight(0),
+        m_tileGrid(nullptr)
+    {
+        // microStudio uses tile ID 0 for blank tiles
+        m_spriteNames.push_back("");
+    }
+    
+    TileMap::~TileMap() {
+        delete[] m_tileGrid;
+    }
+
+    void TileMap::allocateGrid() {
+        m_tileGrid = new int[m_width * m_height];
+        memset(m_tileGrid, 0, m_width * m_height * sizeof(int));
+    }
+
+    void TileMap::addTileInfo(std::string name) {
+        short x = 0, y = 0;
+        size_t pos = name.rfind(":");
+        if (pos != std::string::npos) {
+            sscanf(name.c_str() + pos + 1, "%hd,%hd", &x, &y);
+            name = name.substr(0, pos);
+        }
+        auto it = std::find(m_spriteNames.begin(), m_spriteNames.end(), name);
+        if (it == m_spriteNames.end()) {
+            m_spriteNames.push_back(name);
+        }
+        unsigned index = it - m_spriteNames.begin();
+        m_tiles.push_back({index, x, y});
+    }
+
     Resource* TileMap::loadingHandler(class ms::io::File *file) {
-        return nullptr;
+        std::string filename = file->path().string();
+        std::string text = file->readString();
+        if (text.empty())
+            return nullptr;
+        
+        TileMap* map = new TileMap;
+        try {
+            io::CJSON json;
+            auto root = json.parse(text.c_str());
+            
+            map->m_width = root["width"];
+            map->m_height = root["height"];
+            map->m_blockWidth = root["block_width"];
+            map->m_blockHeight = root["block_height"];
+
+            auto sprites = root["sprites"]; io::CJSONNode sprite;
+            CJSONNode_forEach(sprite, sprites) {
+                std::string spriteTile = (char const*) sprite;
+                map->addTileInfo(spriteTile);
+            }
+            map->allocateGrid();
+
+            int at = 0;
+            auto data = root["data"]; io::CJSONNode datum;
+            CJSONNode_forEach(datum, data) {
+                int tileId = (double) datum;
+                map->m_tileGrid[at++] = tileId;
+            }
+            
+        } catch (io::CJSONError& jsErr) {
+            LOG_MSGF("JSON Error: %s when reading out tilemap \"%s\".\n", jsErr.what(), filename.c_str());
+            delete map;
+            return nullptr;
+        }
+        return map;
     }
 
 }
--- src/resources/ms_tilemap.hpp
+++ src/resources/ms_tilemap.hpp
@@ -6,11 +6,57 @@
 namespace ms {
 namespace res {
 
+    /**
+     * Represents a tile map loaded from a microStudio map JSON.
+     * NOTE: The Y axis of the tile grid is flipped: (0,0) is the bottom-left corner.
+     */
     class TileMap : public Resource {
     public:
+        struct Tile {
+            unsigned id; // index into m_spriteNames
+            short x, y;
+        };
+
+        static const Tile EMPTY_TILE;
+        
+    private:
+        std::vector<std::string> m_spriteNames;
+        std::vector<Tile> m_tiles;
+        
+        int m_width, m_height;
+        int m_blockWidth, m_blockHeight;
+        int* m_tileGrid;
+
+        void addTileInfo(std::string name);
+        void allocateGrid();
+        
+    public:
+        TileMap();
+        ~TileMap();
+
+        /* Getters */
+
+        int getWidth() const { return m_width; }
+        int getHeight() const { return m_height; }
+        int getBlockWidth() const { return m_blockWidth; }
+        int getBlockHeight() const { return m_blockHeight; }
+
+        bool isEmptyTile(Tile tile) const { return tile.id == 0; }
+        inline const Tile& getTile(unsigned x, unsigned y) const;
+        inline std::string_view getTileName(Tile tile) const;
 
         DECLARE_LOADER;
     };
+
+    inline const TileMap::Tile& TileMap::getTile(unsigned x, unsigned y) const {
+        if (x >= m_width || y >= m_height) return EMPTY_TILE;
+        unsigned index = m_tileGrid[x + y * m_width];
+        return m_tiles[index];
+    }
+
+    std::string_view TileMap::getTileName(Tile tile) const {
+        return tile.id >= m_spriteNames.size() ? m_spriteNames[0] : m_spriteNames[tile.id];
+    }
 
 }
 }
